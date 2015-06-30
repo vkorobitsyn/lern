92e329349f7c6de6852a9f5ffd6b1431c5d50874
Vitaliy Korobitsyn <vkorobitsyn@Admins-MacBook-Pro.local>
2015-06-30 22:47:14 +0300

    - created project struct

---
+++ myTest/myTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myTest
+//
+//  Created by Vitaliy Korobitsyn on 6/30/15.
+//  Copyright (c) 2015 Vitaliy Korobitsyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
