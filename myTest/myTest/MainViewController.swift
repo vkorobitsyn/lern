8c4ee63bbc95694163c09bf42a5d6c0a2338a6f9
vkorobitsyn <vkorobitsyn@Admins-MacBook-Pro.local>
2015-12-30 08:36:19 +0200

    add main controller

---
+++ myTest/myTest/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  myTest
+//
+//  Created by Vitaliy Korobitsyn on 12/30/15.
+//  Copyright © 2015 Vitaliy Korobitsyn. All rights reserved.
+//
+
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+}
