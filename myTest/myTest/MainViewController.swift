a8f0f98d067d2c8103339a4c93d1327b420ecc94
vkorobitsyn <vkorobitsyn@Admins-MacBook-Pro.local>
2015-12-30 16:06:20 +0200

    dev SuperWillAppeare

--- myTest/myTest/MainViewController.swift
+++ myTest/myTest/MainViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class MainViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
-            
+        super.viewWillAppear(animated)
     }
 
 }
